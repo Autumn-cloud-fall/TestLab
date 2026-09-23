3a75cf99409880a2945021d06e4c4f13934c91a9
mingxu <3915327039@qq.com>
2026-09-23 15:58:23 +0800

    feature1

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 int main()
 {   
     int a, b;
-    printf("Hello, world!\n");
+    printf("Hello, xuming!\n");
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
     printf("%d + %d is %d\n", a, b, a + b);
