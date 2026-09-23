d7515dc313fe051f6a4812bc7a5baba3bf942f57
mingxu <3915327039@qq.com>
2026-09-23 15:56:58 +0800

    clear2

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main()
-{
+{   
+    int a, b;
     printf("Hello, world!\n");
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("%d + %d is %d\n", a, b, a + b);
+    return 0;
 }
